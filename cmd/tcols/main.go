4618dc45afd07b6950c6a9be900887fc2f294c38
mdm-code <code.madamczyk@gmail.com>
2022-10-14 16:29:46 +0200

    [FEAT] Started working on tcols command
    I started working on the command documentation
    to figure out how I want the command to work
    exactly, so that I don't get stuck somewhere
    along the line.

---
+++ cmd/tcols/main.go
@@ -0,0 +1,31 @@
+/*
+Tcols - add color to text on the terminal.
+
+Tcols reads text from the standard input or a file and writes the colorized
+text to the standard output.
+
+
+Usage:
+
+    tcols [-h] [-o OUTPUT] [-i INPUT]
+
+Options:
+
+
+Example:
+
+Output:
+
+Describe what the output actually is.
+*/
+package main
+
+// TODO: Add a reference on `go doc tcols` in the `README.md` file.
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello, world!")
+}
