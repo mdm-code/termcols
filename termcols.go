31eed8ca1db6aa1dc2f8f920a137ca402c07ed05
mdm-code <code.madamczyk@gmail.com>
2022-10-17 00:50:53 +0200

    [FIX] Fixed the escape sequence error
    Using raw strings introduced problems with
    printing out colored text on the terminal.

--- termcols.go
+++ termcols.go
@@ -7,86 +7,86 @@ import (
 )
 
 const (
-	// Esc sequence \033 works with Bash, Zsh and Dash (appears not to in Ksh
-	// and Csh). There are other escape sequence code representations: \x1b,
-	// \0x1b, \u001b and \e are the ones that I came across. All of these
-	// appear not to work in Bash, and yet they do work in Zsh.
-	Esc = `\033`
+	// Esc octal sequence \033 works with Bash, Zsh and Dash (appears not to in
+	// Ksh and Csh). There are other escape sequence code representations: the
+	// ctrl-key ^[ sequence, unicode \u001b, hexadecimal \x1b, \0x1B, decimal
+	// 27 and \e are the ones that I came across.
+	Esc = "\033"
 
 	// Csi stands for Control Sequence Introducer
-	Csi = Esc + `[`
+	Csi = Esc + "["
 )
 
 // Layer
 const (
-	FG layer = Csi + `38`
-	BG layer = Csi + `48`
+	FG layer = Csi + "38"
+	BG layer = Csi + "48"
 )
 
 // Reset control sequence
-const Reset SgrAttr = Csi + `0m`
+const Reset SgrAttr = Csi + "0m"
 
 // Style
 const (
-	Bold         SgrAttr = Csi + `1m`
-	Faint        SgrAttr = Csi + `2m`
-	Italic       SgrAttr = Csi + `3m`
-	Underline    SgrAttr = Csi + `4m`
-	Blink        SgrAttr = Csi + `5m`
-	Reverse      SgrAttr = Csi + `7m`
-	Hide         SgrAttr = Csi + `8m`
-	Strike       SgrAttr = Csi + `9m`
-	DefaultStyle SgrAttr = Csi + `10m`
+	Bold         SgrAttr = Csi + "1m"
+	Faint        SgrAttr = Csi + "2m"
+	Italic       SgrAttr = Csi + "3m"
+	Underline    SgrAttr = Csi + "4m"
+	Blink        SgrAttr = Csi + "5m"
+	Reverse      SgrAttr = Csi + "7m"
+	Hide         SgrAttr = Csi + "8m"
+	Strike       SgrAttr = Csi + "9m"
+	DefaultStyle SgrAttr = Csi + "10m"
 )
 
 // Normal foreground
 const (
-	BlackFg   SgrAttr = Csi + `30m`
-	RedFg     SgrAttr = Csi + `31m`
-	GreenFg   SgrAttr = Csi + `32m`
-	YellowFg  SgrAttr = Csi + `33m`
-	BlueFg    SgrAttr = Csi + `34m`
-	MagentaFg SgrAttr = Csi + `35m`
-	CyanFg    SgrAttr = Csi + `36m`
-	WhiteFg   SgrAttr = Csi + `37m`
-	DefaultFg SgrAttr = Csi + `39m`
+	BlackFg   SgrAttr = Csi + "30m"
+	RedFg     SgrAttr = Csi + "31m"
+	GreenFg   SgrAttr = Csi + "32m"
+	YellowFg  SgrAttr = Csi + "33m"
+	BlueFg    SgrAttr = Csi + "34m"
+	MagentaFg SgrAttr = Csi + "35m"
+	CyanFg    SgrAttr = Csi + "36m"
+	WhiteFg   SgrAttr = Csi + "37m"
+	DefaultFg SgrAttr = Csi + "39m"
 )
 
 // Bright foreground
 const (
-	BlackBfg   SgrAttr = Csi + `90m`
-	RedBfg     SgrAttr = Csi + `91m`
-	GreenBfg   SgrAttr = Csi + `92m`
-	YellowBfg  SgrAttr = Csi + `93m`
-	BlueBfg    SgrAttr = Csi + `94m`
-	MagentaBfg SgrAttr = Csi + `95m`
-	CyanBfg    SgrAttr = Csi + `96m`
-	WhiteBfg   SgrAttr = Csi + `97m`
+	BlackBfg   SgrAttr = Csi + "90m"
+	RedBfg     SgrAttr = Csi + "91m"
+	GreenBfg   SgrAttr = Csi + "92m"
+	YellowBfg  SgrAttr = Csi + "93m"
+	BlueBfg    SgrAttr = Csi + "94m"
+	MagentaBfg SgrAttr = Csi + "95m"
+	CyanBfg    SgrAttr = Csi + "96m"
+	WhiteBfg   SgrAttr = Csi + "97m"
 )
 
 // Normal background
 const (
-	BlackBg   SgrAttr = Csi + `40m`
-	RedBg     SgrAttr = Csi + `41m`
-	GreenBg   SgrAttr = Csi + `42m`
-	YellowBg  SgrAttr = Csi + `43m`
-	BlueBg    SgrAttr = Csi + `44m`
-	MagentaBg SgrAttr = Csi + `45m`
-	CyanBg    SgrAttr = Csi + `46m`
-	WhiteBg   SgrAttr = Csi + `47m`
-	DefaultBg SgrAttr = Csi + `49m`
+	BlackBg   SgrAttr = Csi + "40m"
+	RedBg     SgrAttr = Csi + "41m"
+	GreenBg   SgrAttr = Csi + "42m"
+	YellowBg  SgrAttr = Csi + "43m"
+	BlueBg    SgrAttr = Csi + "44m"
+	MagentaBg SgrAttr = Csi + "45m"
+	CyanBg    SgrAttr = Csi + "46m"
+	WhiteBg   SgrAttr = Csi + "47m"
+	DefaultBg SgrAttr = Csi + "49m"
 )
 
 // Bright background
 const (
-	BlackBbg   SgrAttr = Csi + `100m`
-	RedBbg     SgrAttr = Csi + `101m`
-	GreenBbg   SgrAttr = Csi + `102m`
-	YellowBbg  SgrAttr = Csi + `103m`
-	BlueBbg    SgrAttr = Csi + `104m`
-	MagentaBbg SgrAttr = Csi + `105m`
-	CyanBbg    SgrAttr = Csi + `106m`
-	WhiteBbg   SgrAttr = Csi + `107m`
+	BlackBbg   SgrAttr = Csi + "100m"
+	RedBbg     SgrAttr = Csi + "101m"
+	GreenBbg   SgrAttr = Csi + "102m"
+	YellowBbg  SgrAttr = Csi + "103m"
+	BlueBbg    SgrAttr = Csi + "104m"
+	MagentaBbg SgrAttr = Csi + "105m"
+	CyanBbg    SgrAttr = Csi + "106m"
+	WhiteBbg   SgrAttr = Csi + "107m"
 )
 
 // SgrAttr corresponds to a SGR (Select Graphic Rendition) control sequence
